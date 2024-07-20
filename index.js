ffa98e17c3bebe2280216c54193c1b1852e18701
baliram123giri <giri710401@gmail.com>
2024-07-20 09:34:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000
+app.use(express.json());
+app.get('/', function (req, res) {
+    res.send("Hello, world!");
+})
+app.listen(port, () => console.log('listening on port ' + port + '...'));  //
