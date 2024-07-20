f3c1c30fdaac42bc082654f0249bf5322c6c9f91
baliram123giri <giri710401@gmail.com>
2024-07-20 17:53:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000
 app.use(express.json());
 app.get('/', function (req, res) {
-    res.send("Finnly done!");
+    res.send("Finnly done ok!");
 })
 app.listen(port, () => console.log('listening on port ' + port + '...'));  //
