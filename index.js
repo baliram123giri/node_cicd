b667942b401c30f5ecc200caca2a07f155224c4f
baliram123giri <giri710401@gmail.com>
2024-07-20 17:50:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000
 app.use(express.json());
 app.get('/', function (req, res) {
-    res.send("Hello, Baliram CICD!");
+    res.send("Finnly done!");
 })
 app.listen(port, () => console.log('listening on port ' + port + '...'));  //
